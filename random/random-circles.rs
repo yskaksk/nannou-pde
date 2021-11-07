eb567a67a630051f9a3b7d77ee74206b2e503a62
yskaksk <yskaksk@gmail.com>
2021-11-08 01:11:07 +0900

    fmt

--- random/random-circles.rs
+++ random/random-circles.rs
@@ -1,6 +1,5 @@
 use nannou::prelude::*;
 
-
 fn main() {
     nannou::app(model).run();
 }
@@ -8,11 +7,7 @@ fn main() {
 struct Model {}
 
 fn model(app: &App) -> Model {
-    let _ = app.new_window()
-        .size(600, 600)
-        .view(view)
-        .build()
-        .unwrap();
+    let _ = app.new_window().size(600, 600).view(view).build().unwrap();
     Model {}
 }
 
@@ -21,6 +16,11 @@ fn view(app: &App, _: &Model, frame: Frame) {
     draw.ellipse()
         .x_y(random_range(-300.0, 300.0), random_range(-300.0, 300.0))
         .w_h(random_range(0.0, 200.0), random_range(0.0, 200.0))
-        .rgba8(random_range(0, 127), random_range(0, 127), random_range(0, 127), random_range(110, 127));
+        .rgba8(
+            random_range(0, 127),
+            random_range(0, 127),
+            random_range(0, 127),
+            random_range(110, 127),
+        );
     draw.to_frame(app, &frame).unwrap();
 }
--- random/shudder.rs
+++ random/shudder.rs
@@ -7,11 +7,7 @@ fn main() {
 struct Model {}
 
 fn model(app: &App) -> Model {
-    app.new_window()
-        .size(600, 600)
-        .view(view)
-        .build()
-        .unwrap();
+    app.new_window().size(600, 600).view(view).build().unwrap();
     Model {}
 }
 
@@ -19,8 +15,15 @@ fn view(app: &App, _: &Model, frame: Frame) {
     let draw = app.draw();
     draw.background().rgb(0.8, 0.8, 0.8);
     draw.ellipse()
-        .x_y(0.0 + random_range(-10.0, 10.0), 0.0 + random_range(-10.0, 10.0))
+        .x_y(
+            0.0 + random_range(-10.0, 10.0),
+            0.0 + random_range(-10.0, 10.0),
+        )
         .radius(150.0)
-        .rgb8(34 + random_range(0, 60), 64 + random_range(0, 60), 4 + random_range(0, 60));
+        .rgb8(
+            34 + random_range(0, 60),
+            64 + random_range(0, 60),
+            4 + random_range(0, 60),
+        );
     draw.to_frame(app, &frame).unwrap();
 }
