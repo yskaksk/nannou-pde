7131a85eb4dafafa4537e67b7ca7144d8df62fc2
yskaksk <yskaksk@gmail.com>
2021-11-11 22:42:10 +0900

    grid_1d追加

---
+++ grid/grid_1d.rs
@@ -0,0 +1,21 @@
+use nannou::prelude::*;
+
+fn main() {
+    nannou::sketch(view).run();
+}
+
+fn view(app: &App, frame: Frame) {
+    let draw = app.draw();
+    let win = app.window_rect();
+    draw.background().color(WHITE);
+    for i in 0..10 {
+        let x = map_range(i, 0, 9, win.left(), win.right());
+        let cs = map_range(x, 0.0, win.right(), 0.0, 255.0);
+        draw.ellipse()
+            .x_y(x, 0.0)
+            .radius(10.0)
+            .rgb8(cs.floor() as u8, 0, (255.0 - cs).floor() as u8);
+    }
+    draw.to_frame(app, &frame).unwrap();
+}
+
