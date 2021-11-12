8a3d06386279e2c9f676f989d4234d015cc66a95
yskaksk <yskaksk@gmail.com>
2021-11-12 21:37:35 +0900

    ちょっと修正

--- color/gradient_hsb.rs
+++ color/gradient_hsb.rs
@@ -6,18 +6,20 @@ fn main() {
 
 fn view(app: &App, frame: Frame) {
     let draw = app.draw();
-    draw.background().hsv(0.0, 0.0, 1.0);
-    let win = app.window_rect();
-    let n = 300;
-    let red_hue = 0.0;
-    let blue_hue = 240.0 / 360.0;
-    for i in 0..n {
-        let x = map_range(i, 0, n-1, win.left(), win.right());
-        let hue = map_range(i, 0, n-1, red_hue, blue_hue);
-        draw.rect()
-            .x_y(x, 0.0)
-            .w_h(win.w() / n as f32 + 1.0, win.h())
-            .hsv(hue, 1.0, 1.0);
+    if frame.nth() == 0 {
+        draw.background().hsv(0.0, 0.0, 1.0);
+        let win = app.window_rect();
+        let n = 300;
+        let red_hue = 0.0;
+        let blue_hue = 240.0 / 360.0;
+        for i in 0..n {
+            let x = map_range(i, 0, n-1, win.left(), win.right());
+            let hue = map_range(i, 0, n-1, red_hue, blue_hue);
+            draw.rect()
+                .x_y(x, 0.0)
+                .w_h(win.w() / n as f32 + 1.0, win.h())
+                .hsv(hue, 1.0, 1.0);
+        }
     }
     draw.to_frame(app, &frame).unwrap();
 }
